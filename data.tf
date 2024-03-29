453b43c88c2b870db8a924a8ca30c90a3b54b823
purna <purnaraomba@gmail.com>
2024-03-29 09:42:19 +0530

    Roboshop Update

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
-#data "aws_ssm_parameter" "user" {
-#  name = "${var.env}.rds.user"
-#}
-#
-#data "aws_ssm_parameter" "pass" {
-#  name = "${var.env}.rds.pass"
-#}
+data "aws_ssm_parameter" "user" {
+  name = "${var.env}.rds.user"
+}
+
+data "aws_ssm_parameter" "pass" {
+  name = "${var.env}.rds.pass"
+}
--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
-#variable "tags" {}
-#variable "env" {}
-#variable "subnet_ids" {}
-#variable "engine" {}
-#variable "engine_version" {}
-#
-#variable "database_name" {
-#  default = "dummy"
-#}
-#variable "backup_retention_period" {}
-#variable "preferred_backup_window" {}
+variable "tags" {}
+variable "env" {}
+variable "subnet_ids" {}
+variable "engine" {}
+variable "engine_version" {}
+
+variable "database_name" {
+  default = "dummy"
+}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
